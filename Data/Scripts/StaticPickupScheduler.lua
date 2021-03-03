d1bd2ce16f7de0d999df0e6a857413502144c254
Andrew Zhilin <andrew_zhilin@yahoo.com>
2021-03-03 16:11:10 +0300

    Update StaticPickupScheduler.lua

--- Data/Scripts/StaticPickupScheduler.lua
+++ Data/Scripts/StaticPickupScheduler.lua
@@ -1,26 +1,43 @@
 local StaticPickupScheduler = {}
 StaticPickupScheduler.__index = StaticPickupScheduler
-local MAX_SPAWNS_PER_TICK = 10
+local MAX_SPAWNS_PER_TICK = 3
 
 local Heap = _G.req("_Heap")
 
-local _sp_scheduler = Heap.New(function(lhs, rhs) return lhs[1] < rhs[1] end)
+--[[ NOTE:
+    StaticPickupScheduler has nothing that is `Static` specific. It can be used
+    for an any scheduled spawns.
+ ]]
 
-function StaticPickupScheduler.Add(fire_at, template, parent, position)
-    _sp_scheduler:Push({fire_at, template, parent, position})
+local _sp_scheduler = Heap.New(function(lhs, rhs)
+    return lhs[1] < rhs[1] -- assume 1-st array element is a `fire-at` time
+ end)
+
+function StaticPickupScheduler.Add(fire_at, template, parent, position, rotation, scale)
+    local entry = {
+        [1]=fire_at,
+        [2]=template,
+        parent=parent,
+        position=position,
+        rotation=rotation,
+        scale=scale
+    }
+    _sp_scheduler:Push(entry)
 end
 
 local function _sp_scheduler_worker()
-    local _dt = Task.Wait()
     local now = time()
     for _=1, MAX_SPAWNS_PER_TICK do
         local entry = _sp_scheduler:Peek()
-        if not entry or entry[1] > now then
+        if _sp_scheduler:IsEmpty() or entry[1] > now then
             break
         end
         _sp_scheduler:Pop()
-        local _, template, parent, position = table.unpack(entry)
-        World.SpawnAsset(template, {parent=parent, position=position})
+        local template = assert(entry[2])
+        -- Core will issue warning if we dont clear extra fields:
+        entry[1] = nil
+        entry[2] = nil
+        World.SpawnAsset(template, entry)
     end
 end
 StaticPickupScheduler._task = Task.Spawn(_sp_scheduler_worker)
