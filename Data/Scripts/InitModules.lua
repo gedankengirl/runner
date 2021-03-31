54c02ae430c3dea0a4568c04774033f95744e8d4
Andrew Zhilin <andrew_zhilin@yahoo.com>
2021-03-31 15:25:42 +0300

    add warning for long module tests

--- Data/Scripts/InitModules.lua
+++ Data/Scripts/InitModules.lua
@@ -15,6 +15,12 @@ _G.req = function(moduleName)
     if not muid then
         error("unknown module: '" .. moduleName .. "'", 2)
     end
-    return require(muid)
+    local t1 = os.clock()
+    local mod = require(muid)
+    local dt = os.clock() - t1
+    if dt >= 0.005 then
+        warn(string.format("[%s] load time: %.1g ms", moduleName, dt*1000))
+    end
+    return mod
 end
 
