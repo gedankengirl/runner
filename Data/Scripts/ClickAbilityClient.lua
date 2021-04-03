6e0fbfe711f199d3d8003942bd708477ee17116f
Andrew Zhilin <andrew_zhilin@yahoo.com>
2021-04-04 01:23:06 +0300

    fix click ability bubbles for all clients

--- Data/Scripts/ClickAbilityClient.lua
+++ Data/Scripts/ClickAbilityClient.lua
@@ -2,7 +2,6 @@ local Maid = _G.req("_Maid")
 local _maid = Maid.New(script)
 local REvents = _G.req("ReliableEvents")
 local P = _G.req("Protocols")
-
 local CLICK_BOOSTER_DB_ID = 0
 
 _maid = Maid.New(script)
@@ -12,6 +11,8 @@ assert(Environment.IsClient())
 assert(not script.isNetworked)
 
 _maid.ability_execute = ABILITY.executeEvent:Connect(function(_ability)
-    -- player, boosterId, pos
-    REvents.Broadcast(P.STATIC.StaticPickup, LOCAL_PLAYER, CLICK_BOOSTER_DB_ID, Vector2.ZERO)
+    if ABILITY.owner == LOCAL_PLAYER then
+        -- player, boosterId, pos
+        REvents.Broadcast(P.STATIC.StaticPickup, LOCAL_PLAYER, CLICK_BOOSTER_DB_ID, Vector2.ZERO)
+    end
 end)
